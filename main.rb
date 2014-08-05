f7b68845adcb8984596d52a3c720c0e3e3c87b2c
Doni <doni@kudelabs.com>
2014-08-05 17:55:21 +0800

    Finish test.

--- main.rb
+++ main.rb
@@ -1,3 +1,29 @@
 #!/usr/bin/env ruby
 
+class SomeClass
+
+  attr_accessor :field1
+  
+  def method1
+    self.field1 = 1
+  end
+
+  def method2
+    field1 = 2
+  end
+
+  def method3
+    @field1 = 3
+  end
+
+end
+
+s1 = SomeClass.new
+s1.method1
+puts "After method1 #{s1.field1}."
+s1.method2
+puts "After method2 #{s1.field1}."
+s1.method3
+puts "After method3 #{s1.field1}."
+
 
