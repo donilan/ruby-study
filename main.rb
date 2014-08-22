68162d9046f286ffd80aa7d111aa8eeaab35746c
Doni <doni@kudelabs.com>
2014-08-22 12:06:01 +0800

    testing for method_missing.

--- main.rb
+++ main.rb
@@ -1,3 +1,17 @@
 #!/usr/bin/env ruby
 
+class DoNothing
+  def method_missing(m, *args, &block)
+    puts "#{m} #{args} #{block}"
+  end
+end
+
+dog = DoNothing.new
+dog.wow
+dog.walk_to 'GuangZhou'
+dog.sleep do
+  puts "Hi"
+end
+
+
 
