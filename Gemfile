f747fd2da800d7071f8561fa938b7cac002e62ab
Doni <d@ii2d.com>
2014-10-11 16:35:02 +0800

    Finish test.

--- Gemfile
+++ Gemfile
@@ -1,5 +1,3 @@
 source 'http://ruby.taobao.org'
 
-gem 'eventmachine'
-gem 'em-http-request'
-gem 'ruby-prof'
+gem 'metriks'
--- main.rb
+++ main.rb
@@ -1,2 +1,11 @@
 #!/usr/bin/env ruby
 
+require 'metriks'
+
+while true
+  ran = Random.rand(0.1..1.5).round(2)
+  meter = Metriks.meter('requests')
+  meter.mark
+  puts "#{meter.count} rate: #{meter.one_minute_rate}, #{meter.five_minute_rate}, #{meter.fifteen_minute_rate} ran: #{ran}"
+  sleep(ran)
+end
