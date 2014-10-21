666cea06218ab7670c5e1bc892d9476fc527e8f3
Doni Leung <doni@kudelabs.com>
2014-10-21 17:26:13 +0800

    Finish.

--- main.rb
+++ main.rb
@@ -1,2 +1,31 @@
 #!/usr/bin/env ruby
 
+class App
+  
+  def initialize app, number
+    @number = number
+    @app = app
+  end
+
+  def call
+    puts "#{@number} bean called"
+    @app.call if @app
+  end
+
+  def to_s
+    "No: #{@number}"
+  end
+end
+
+apps = []
+
+5.times do |i|
+  apps << proc {|app| App.new app, i}
+end
+
+app = App.new nil, 'leader'
+new_app = apps.reverse.inject(app) { |a, e| e[a]}
+
+require 'pry'
+binding.pry
+
