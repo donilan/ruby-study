65a2412c9b7c0b9b7caa198a4445b8e5ed86dc10
Doni Leung <doni@kudelabs.com>
2014-10-27 18:47:34 +0800

    Finish

--- main.rb
+++ main.rb
@@ -1,2 +1,13 @@
 #!/usr/bin/env ruby
 
+require 'mechanize'
+agent = Mechanize.new {|a| 
+  # a.ssl_version = 'SSLv3'
+  # a.ssl_version = :TLSv1
+  a.agent.http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+}
+page = agent.get("https://gtldresult.icann.org/application-result/applicationstatus/viewstatus")
+
+puts page
+
+
