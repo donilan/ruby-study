a98441b79207d3f9ae053550b9fc5c250d1688d3
Doni Leung <doni@kudelabs.com>
2014-12-09 11:42:58 +0800

    Finish

--- main.rb
+++ main.rb
@@ -1,2 +1,7 @@
 #!/usr/bin/env ruby
 
+
+Struct1 = Struct.new :prop1, :prop2
+struct1_array = []
+100000.times { |n| struct1_array << Struct1.new("prop1 #{n}", n)}
+puts "Struct1 array size is: #{Marshal.dump(struct1_array).size}"
