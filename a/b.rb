f8b97894d1af75c463197ec86ac7f4bf9428ef0d
Doni Leung <doni@kudelabs.com>
2015-11-16 17:02:21 +0800

    Finished test.

---
+++ a/b.rb
@@ -0,0 +1,5 @@
+module A
+  class B
+    puts "Loading class B"
+  end
+end
--- main.rb
+++ main.rb
@@ -1,2 +1,10 @@
 #!/usr/bin/env ruby
+$:.unshift File.dirname(__FILE__)
+module A
+  puts "Loading module A"
+  autoload :B, 'a/b'
+  puts "Loaded module A"
+end
 
+A
+A::B.new
